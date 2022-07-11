c31c5df52b899aca042f524d8d88206b7c292586
dengfa <dengfa@bytedance.com>
2022-07-11 12:12:17 +0800

    first commit

---
+++ 1.两数之和.kt
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=kotlin
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+
+    }
+}
+// @lc code=end
+
